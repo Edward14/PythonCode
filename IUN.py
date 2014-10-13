a5e95d848f4f36ae2aa4ad198f95189d4bdc9979
Edward14 <smallsodagreen@gmail.com>
2014-10-13 14:40:33 +0800

    IUN
    new if

---
+++ IUN.py
@@ -0,0 +1,8 @@
+x = int(input("PLZ give me the sucking Number!\nJust from 1~3 :\n"));
+
+if x == 1:
+	print("ok,This one is my favorite!")
+elif x == 2:
+	print("You give me the wrong number.")
+else:
+	print("Can you try again???????????????")
